2765a0b9f6ce290022baadb3019d8c84de1e164a
Isaac <isaac.cruz@element.com.mx>
2016-08-25 16:27:25 -0500

    Finished

--- src/routes.php
+++ src/routes.php
@@ -12,16 +12,24 @@ $app->get('/', function (Request $req, Response $res) {
 $app->post('/process', function (Request $request, Response $response) {
 	$data = $request->getParsedBody();
 
+	// Recibimos la variables
 	$grid = isset($data['grid']) ? $data['grid'] : array();
 	$quantity = isset($data['quantity']) ? (int)$data['quantity']: "";
 	$movements = isset($data['movements']) ? $data['movements'] : array();
 
+	// Validación de parametros
 	if(count($grid) != 4 && count($movements) != $quantity){
 		$error = array();
 		$error[] = "No se cumplen con las reglas iniciales";
 		return $response->withJson($error, 201);
 	}
+
+	// Convertimos el grid en array
+	$grid = gridToArray($grid);
+
+	// Iteramos por cada movimiento recibido
 	foreach ($movements as $key => $movement) {
+		// Ecogemos el tipo de movimiento
 		switch ($movement) {
 			case 'Izquierda':
 				$grid = moveLeftGrid($grid);
@@ -37,15 +45,15 @@ $app->post('/process', function (Request $request, Response $response) {
 				break;
 		}
 	}
-	$grid = implode("\r\n", $grid);
-
+	$grid = gridToString($grid);
 	return $response->withJson(array("result"=>true, "grid" => $grid), 201);
 });
 
+// Aquí movemos el grid a la derecha
 function moveRigthGrid(array $grid)
 {
-	foreach ($grid as $key => $line) {
-		$numbers = moveZeroToLeft(explode(" ", $line));
+	foreach ($grid as $key => $numbers) {
+		$numbers = moveZeroLeft($numbers);
 		if($numbers[3] == $numbers[2]){
 			$numbers[3] = $numbers[3] + $numbers[2];
 			$numbers[2] = $numbers[1];
@@ -61,15 +69,16 @@ function moveRigthGrid(array $grid)
 			$numbers[1] = $numbers[1] + $numbers[0];
 			$numbers[0] = 0;
 		}
-		$grid[$key] = implode(" ", $numbers);
+		$grid[$key] = $numbers;
 	}
 	return $grid;
 }
 
+// Aquí movemos el grid a la izquierda
 function moveLeftGrid(array $grid)
 {
-	foreach ($grid as $key => $line) {
-		$numbers = moveZeroToRigth(explode(" ", $line));
+	foreach ($grid as $key => $numbers) {
+		$numbers = moveZeroRigth($numbers);
 		if($numbers[0] == $numbers[1]){
 			$numbers[0] = $numbers[0] + $numbers[1];
 			$numbers[1] = $numbers[2];
@@ -85,22 +94,91 @@ function moveLeftGrid(array $grid)
 			$numbers[2] = $numbers[2] + $numbers[3];
 			$numbers[3] = 0;
 		}
-		$grid[$key] = implode(" ", $numbers);
+		$grid[$key] = $numbers;
 	}
 	return $grid;
 }
 
+// Aquí movemos el grid hacia arriba
 function moveUpGrid(array $grid)
 {
+	$grid = moveZeroDown($grid);
+	for ($i=0; $i < count($grid); $i++) { 
+		if($grid[0][$i] == $grid[1][$i]){
+			$grid[0][$i] = $grid[0][$i] + $grid[1][$i];
+			$grid[1][$i] = $grid[2][$i];
+			$grid[2][$i] = $grid[3][$i];
+			$grid[3][$i] = 0;
+		}
+		if($grid[1][$i] == $grid[2][$i]){
+			$grid[1][$i] = $grid[1][$i] + $grid[2][$i];
+			$grid[2][$i] = $grid[3][$i];
+			$grid[3][$i] = 0;
+		}
+		if($grid[2][$i] == $grid[3][$i]){
+			$grid[2][$i] = $grid[2][$i] + $grid[3][$i];
+			$grid[3][$i] = 0;
+		}
+	}
 	return $grid;
 }
 
+// Aquí movemos el grid hacia abajo
 function moveDownGrid(array $grid)
 {
+	$grid = moveZeroUp($grid);
+	for ($i=0; $i < count($grid); $i++) { 
+		if($grid[3][$i] == $grid[2][$i]){
+			$grid[3][$i] = $grid[3][$i] + $grid[2][$i];
+			$grid[2][$i] = $grid[1][$i];
+			$grid[1][$i] = $grid[0][$i];
+			$grid[0][$i] = 0;
+		}
+		if($grid[2][$i] == $grid[1][$i]){
+			$grid[2][$i] = $grid[2][$i] + $grid[1][$i];
+			$grid[1][$i] = $grid[0][$i];
+			$grid[0][$i] = 0;
+		}
+		if($grid[1][$i] == $grid[0][$i]){
+			$grid[1][$i] = $grid[1][$i] + $grid[0][$i];
+			$grid[0][$i] = 0;
+		}
+	}
 	return $grid;
 }
 
-function moveZeroToRigth(array $numbers)
+// Movemos toodos los ceros hacia abajo
+function moveZeroDown(array $grid)
+{
+	for ($i=0; $i < 4; $i++) { 
+		$numbers = array_column($grid, $i);
+		$columns = moveZeroRigth($numbers);
+
+		$grid[0][$i] = $columns[0];
+		$grid[1][$i] = $columns[1];
+		$grid[2][$i] = $columns[2];
+		$grid[3][$i] = $columns[3];
+	}
+	return $grid;
+}
+
+// Movemos toodos los ceros hacia arriba
+function moveZeroUp(array $grid)
+{
+	for ($i=0; $i < 4; $i++) { 
+		$numbers = array_column($grid, $i);
+		$columns = moveZeroLeft($numbers);
+
+		$grid[0][$i] = $columns[0];
+		$grid[1][$i] = $columns[1];
+		$grid[2][$i] = $columns[2];
+		$grid[3][$i] = $columns[3];
+	}
+	return $grid;
+}
+
+// Movemos toodos los ceros hacia la derecha
+function moveZeroRigth(array $numbers)
 {
 	$return = array();
 	for ($i = 0; $i < count($numbers); $i++) {
@@ -114,7 +192,8 @@ function moveZeroToRigth(array $numbers)
 	return $return;
 }
 
-function moveZeroToLeft(array $numbers)
+// Movemos toodos los ceros hacia la izquierda
+function moveZeroLeft(array $numbers)
 {
 	$return = array();
 	for ($i = 0; $i < count($numbers); $i++) {
@@ -126,4 +205,23 @@ function moveZeroToLeft(array $numbers)
 		array_unshift($return, "0");
 	}
 	return $return;
+}
+
+
+// Convertimos el grid a string
+function gridToString($grid){
+	foreach ($grid as $key => $numbers) {
+		$grid[$key] = implode(" ", $numbers);
+	}
+	return implode("\r\n", $grid);
+}
+
+// Convertimos el grid a array
+function gridToArray(array $grid)
+{
+	$result = array();
+	foreach ($grid as $key => $line) {
+		$result[] = explode(" ", $line);
+	}
+	return $result;
 }
